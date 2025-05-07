3cd1fbdb0741b94258cb8defab3fe6ea039c6a7b
manju766 <mallikarjunmalli51946@gmail.com>
2025-05-07 11:00:20 +0530

    Added Employee class and Main driver

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Create an employee object
+        Employee emp = new Employee("John Doe", "2000-01-15", "EMP001", 50000.0);
+
+        // Display employee details
+        emp.displayEmployeeDetails();
+    }
+}
